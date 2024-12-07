a226db353f118c6e2f1996e44011e3716d8d2834
kyoichiroT <kyoichirotomatsu@gmail.com>
2024-12-07 11:33:53 +0900

    dtoの変更

--- src/feature/match-data/match-data.repository.ts
+++ src/feature/match-data/match-data.repository.ts
@@ -6,9 +6,6 @@ export class MatchDataRepository {
     
     async getWinRatesFromS3(): Promise<WinRate[]> {
         // S3からデータを取得するロジックをここに記述
-        return [
-            { team: 'Team C', winRate: 80 },
-            { team: 'Team D', winRate: 55 },
-        ];
+        return [];
     }
 }
