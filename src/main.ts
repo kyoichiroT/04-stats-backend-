cc75703c25f3c02617540d80233a5ad6006ebcaa
kyoichiroT <kyoichirotomatsu@gmail.com>
2024-12-30 11:47:28 +0900

    'corsの設定'

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,12 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // corsの設定
+  app.enableCors({
+    origin: 'http://localhost:3000', // 許可するオリジンを指定
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
     .setDescription('The API description')
@@ -16,6 +22,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
